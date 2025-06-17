f5e4f73bb907ca11710ae3b65656ea3cbed86158
juan galeano <jgaleano@quadiontech.com>
2025-06-17 20:08:01 -0300

    cuadricula para pantalla

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import pygame
 
 
 pygame.init()
-
-pantalla = pygame.display.set_mode((800, 600))
+ancho = 800
+alto = 600
+pantalla = pygame.display.set_mode((ancho, alto))
 fondo = pygame.image.load("resources\water.png")
 fondo = pygame.transform.scale(fondo,(800,600))
 pygame.display.set_icon(pygame.image.load("resources\ship.png"))
 pygame.display.set_caption("BATALLA NAVAL")
-
+ancho_celda = ancho // 10
+alto_celda = alto // 10
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
@@ -17,6 +19,15 @@ while True:
 
     pantalla.blit(fondo, (0, 0))
     pygame.display.flip()
+
+    for i in range(1, 10):
+        y = i * alto_celda
+        pygame.draw.line(pantalla, (255, 255, 255), (0, y), (ancho, y), 2)
+
+    
+    for j in range(1, 10):
+        x = j * ancho_celda
+        pygame.draw.line(pantalla, (255, 255, 255), (x, 0), (x, alto), 2)
 
     
     
